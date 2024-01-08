422465940970ff767b7e41c3f132a8eb7c84821f
samuelbamueldamuel <samdinardi@gmail.com>
2024-01-08 13:43:36 -0500

    origin

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Task(models.Model):
+    key = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    point = models.IntegerField()
+    chosen = models.BooleanField(default=False)
+
+class History(models.Model):
+    key = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    point = models.IntegerField()
+    accepted = models.BooleanField()
